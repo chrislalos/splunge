427ff8474d4d9b2714e92afd1d072d58fd6ae3ff
Chris Lalos <beantaxi@gmail.com>
2016-06-02 14:58:45 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(name='splunge',
+                 version='0.1',
+                 url='https://github.com/beantaxi/dev/tree/master/py/splunge',
+                 install_requires=['gunicorn', 'jinja2'],
+                 packages=['splunge'])
