ca98d28a0d1e0bf87280789863f8cb13ecf3f340
Bean Taxi <beantaxi@gmail.com>
2016-11-28 14:19:11 -0600

    no message

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 import setuptools
 
+
 setuptools.setup(name='splunge',
-                 version='0.1',
-                 url='https://github.com/beantaxi/dev/tree/master/py/splunge',
-                 install_requires=['gunicorn', 'jinja2'],
-                 packages=['splunge'])
+                 description='Relentelessly simple Python web framework.',
+                 keywords='python web wsgi gunicorn django pyramid mako',
+                 version='0.0.1',
+                 url='https://github.com/beantaxi/splunge',
+                 author='Chris Lalos',
+                 author_email='chris.lalos@gmail.com',
+                 install_requires=['gunicorn'],
+                 packages=setuptools.find_packages('src'),
+                 package_dir={'': 'src'})
+#                 packages=setuptools.find_packages(exclude=['tests']))
