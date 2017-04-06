e97ff49b9bdf38ee6cd638adbf39e7f1e4a0d268
Bean Taxi <beantaxi@gmail.com>
2017-04-06 05:49:46 -0500

    no message

--- publish.sh
+++ publish.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+
 if [[ -z SSH_AUTH_SOCK || -z SSH_AGENT_PID ]]; then
 	>&2 echo "Please run ssh-agent and then rerun this script"
 	exit 1
 fi
 
 home=$(getdir "${BASH_SOURCE[0]}")
-export PYTHONPATH=$PYTHONPATH:$home
-echo PYTHONPATH=$PYTHONPATH
+cd "$home"
+# export PYTHONPATH=$PYTHONPATH:$home
+# echo PYTHONPATH=$PYTHONPATH
 commitMsg=${1:-'no message'}
 git -C "$home" commit -am "$commitMsg" && git push; python3 -m setup clean sdist upload -r sparktools
