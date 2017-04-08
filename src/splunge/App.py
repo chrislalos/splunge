6603198f9f32c6037cf1359e2e800a655ad4f986
Bean Taxi <beantaxi@gmail.com>
2017-04-08 14:04:23 -0500

    Pretty serious code cleanup; no new features

--- src/splunge/App.py
+++ src/splunge/App.py
@@ -105,14 +105,25 @@ def validateMethod (method, methods):
 
 class Response:
 	def __init__ (self):
-		self.text = ""
+		self.text = ''
 		self.statusCode = 200
-		self.statusMessage = "OK"
+		self.statusMessage = 'OK'
 		self.headers = []
 	
 	def addHeader (self, *args):
 		headerTuple = argsToTuple(*args, length=2)
 		self.headers.append(headerTuple)
+
+	def getStatus (self):
+		statusLine = '{} {}'.format(self.statusCode, self.statusMessage)
+		return statusLine
+	
+	def handleError (self, statusCode, shortMsg, longMsg):
+		self.setStatus(statusCode, shortMsg)
+		self.setContentType('text/plain')
+		self.addHeader('Warning', shortMsg)
+		self.response.body = longMsg
+		self.exc_info = sys.exc_info()
 
 	def hasHeader (self, name):
 		found = False
@@ -123,11 +134,19 @@ class Response:
 				break
 		return found
 
-
 	def redirect (self, url):
 		self.statusCode = 303
-		self.statusMessage = "Response Page to POST"
-		self.addHeader("Location", url)
+		self.statusMessage = 'Response Page to POST'
+		self.addHeader('Location', url)
+
+	def setContentType (self, contentType):
+		self.addHeader('Content-type', contentType)
+
+	def setStatus (self, statusCode, statusMessage):
+		self.statusCode = statusCode
+		self.statusMessage = statusMessage
+
+	
 
 
 ###########################################################################################
@@ -135,6 +154,10 @@ class Response:
 # Here's what splunge does.
 #
 # For url http://yourhost/name
+#
+# Zeroth: Splunge checks to see if your file refers to static content (ie a simple text file,
+#         html file, jpeg/png, etc). If so, it streams the contents back in the response, setting
+#         Content-type appropriately.
 #
 # First: Splunge looks for a file named name.py
 #        If name.py is found, splunge loads the module using magic loader, and executes it.
@@ -161,61 +184,38 @@ class Application ():
 		self.startResponse = start_response
 		self.response = Response()
 		self.path = self.env['PATH_INFO'].strip()
-		print("path=" + self.path)
+		print('path=' + self.path)
 		try:
 			self.handleRequest()
-		except FaviconEx as ex:
-			errmsg = "The Favicon feature (favicon.ico) is not supported here, my friend"
-			headers = [
-				('Content-type', 'text/plain'),
-				('Warning', errmsg)
-			]
-			self.startResponse("410 {}".format(errmsg), headers, sys.exc_info())
-			self.response.body = errmsg
 		except FileNotFoundError as ex:
-			print(ex)
-			headers = [
-				('Content-type', 'text/plain'),
-			]
-			self.startResponse('404 File Not Found', headers, sys.exc_info())
-			self.response.body = str(ex)
+			shortMsg = 'File Not Found'
+			longMsg = "We're sorry, we were unable to find {}".format(self.path)
+			self.handleError(404, shortMsg, longMsg)
 		except GeneralClientEx as ex:
-			print(ex)
-			headers = [
-				('Content-type', 'text/plain'),
-				('Warning', ex.getWarningHeaderValue())
-			]
-			self.startResponse('400 Oops', headers, sys.exc_info())
-			self.response.body = str(ex)
+			shortMsg = 'General Client Error'
+			longMsg = 'There was an error on the client side'
+			self.handleError(400, shortMsg, longMsg)
 		except InvalidMethodEx as ex:
-			print(ex)
-			headers = [
-				("Content-type", "text/plain"),
-				("Allow", ex.getAllowHeaderValue())
-			]
-			self.startResponse("405", headers, sys.exc_info())
-			self.response.body = str(ex)
+			shortMsg = 'Method Not Allowed'
+			allowHeaderValue = ex.getAllowHeaderValue()
+			longMsg = 'Your HTTP request used an invalid method. Here is a list of allowed methods: {}'.format(allowHeaderValue)
+			handleError(405, shortMsg, longMsg)
+			self.response.addHeader('Allow', ex.getAllowHeaderValue())
 		except jinja2.exceptions.TemplateNotFound as ex:
-			msg = "Template file not found: {}".format(ex.message)
-			headers = [
-				("Content-type", "text/plain"),
-				("Warning", msg)
-			]
-			self.startResponse("404", headers, sys.exc_info())
-			self.response.body = msg 
+			shortMsg = 'Template file not found'
+			longMsg = "We're sorry, we were unable to find {}".format(self.path)
+			self.handleError(404, shortMsg, longMsg)
 		except Exception as ex:
-			traceback.print_exc()
-			headers = [
-				("Content-type", "text/plain")
-			]
-			self.startResponse("500", headers, sys.exc_info())
-			self.response.body = "An error has occured on the server."
+			shortMsg = 'General Server Error'
+			longMsg = 'An error has occured on the server.'
+			self.handleError(500, shortMsg, longMsg)
 		else:
 			# Check if content type header was explicitly set. If it was not then set it to text/html
 			if not self.response.hasHeader('Content-Type'):
 				self.response.addHeader('Content-Type', 'text/html')
-			status = "{} {}".format(self.response.statusCode, self.response.statusMessage)
-			self.startResponse(status, self.response.headers)
+		statusLine = self.response.getStatus()
+		response()
+
 
 
 	def __iter__ (self):
@@ -229,7 +229,7 @@ class Application ():
 	def createGetArgs (self):
 		args = {}
 		qs = self.env['QUERY_STRING']
-		print("QUERY_STRING={}".format(qs))
+		print('QUERY_STRING={}'.format(qs))
 		d = urllib.parse.parse_qs(qs)
 		pprint(d)
 		for key, v in d.items():
@@ -241,7 +241,6 @@ class Application ():
 				args[key] = value
 		return args
 					
-
 
 	# http://wsgi.tutorial.codepoint.net/parsing-the-request-post
 	def createPostArgs (self):
@@ -282,39 +281,38 @@ class Application ():
 	# This is the magic, which allows python modules in a splunge app to have 'global'
 	# functions without doing any explicit imports. Who the heck wants to do imports?
 	#
-	# Note there is more value here than mere namespace pollution. E.g., instead of 
+	# There is more value here than mere namespace pollution. E.g., instead of 
 	# maintaining a response object, and having to call response.headers.append(), you 
 	# can just call addHeader(). Nice.
 	#
 	def enrichModule (self, module):
 		reqMethod = self.env['REQUEST_METHOD']
-		# Assigning a null lambda is apparently an acceptable Python idiom for creating an anonymous object
 		module.http = self.createHttpObject()
 		module.response = self.response
-		module.addHeader = lambda name, value: self.response.addHeader(name, value)
+		module.addHeader =      lambda name, value: self.response.addHeader(name, value)
 		module.validateMethod = lambda validMethods: validateMethod(reqMethod, validMethods)
-		module.redirect = lambda url: { self.response.redirect(url) }
+		module.redirect =       lambda url: { self.response.redirect(url) }
 		module.setContentType = lambda contentType: self.response.addHeader('Content-type', contentType)
 		# import exceptions
 
 
 	# Translate the path from the URL, to the local file or resource being referred to
 	def getLocalPath (self):
-		relPath = "{}{}".format(os.getcwd(), self.path)
+		relPath = '{}{}'.format(os.getcwd(), self.path)
 		return relPath		
 
 
 	# Append .py to the path, then append the path to the working dir, and that's the python path
 	def inferPythonPath (self):
 		localPath = self.getLocalPath()
-		pythonPath = "{}.py".format(localPath)
+		pythonPath = '{}.py'.format(localPath)
 		return pythonPath		
 
 
 	# Get the local version of the http path, append .pyp, and that's the template path
 	def inferTemplatePath (self):
 		localPath = self.getLocalPath()
-		templatePath = "{}.pyp".format(localPath)
+		templatePath = '{}.pyp'.format(localPath)
 		return templatePath		
 
 
@@ -323,41 +321,40 @@ class Application ():
 			self.handleShortcutResponse(args)
 		else:
 			self.handleTemplateFile(args)
-#  			print("template path was not found")
-#  			self.response.headers.append(('Content-Type', 'text/plain'))
-#  			for name in sorted(self.args):
-#  				value = self.args.get(name, '')
-#  				if not callable(value):
-#  					self.response.text += "{} = {}\n".format(name, value)
+
+
+	def handleError (self, statusCode, shortMsg, longMsg):
+		traceback.print_exc()
+		self.response.handleError(statusCode, shortMsg, longMsg)
 
 
 	def handleRequest (self):
 		if Application.isDefault(self.path):
-			print("*** handleDefaultPath()")
+			print('*** handleDefaultPath()')
 			self.handleDefaultPath()
 		elif Application.isFavicon(self.path):
-			print("*** handleFavicon()")
+			print('*** handleFavicon()')
 			self.handleFavicon()
 		elif self.isPythonFile():
-			print("*** handlePythonFile()")
+			print('*** handlePythonFile()')
 			localPath = self.getLocalPath()
 			self.handlePythonFile(localPath)
 		elif Application.isTemplateFile(self.path):
-			print("*** handleTemplateFile()")
+			print('*** handleTemplateFile()')
 			localPath = self.getLocalPath()
 			self.handleTemplateFile(localPath)
 		elif self.isStaticContent():
-			print("*** handleStaticContent()")
+			print('*** handleStaticContent()')
 			self.handleStaticContent()
 		else:
 			pythonPath = self.inferPythonPath()
 			if os.path.isfile(pythonPath):
-				print("*** handlePythonFile()")
+				print('*** handlePythonFile()')
 				self.handlePythonFile(pythonPath)
 			else:
 				templatePath = self.inferTemplatePath()
 				if os.path.isfile(templatePath):
-					print("*** handleTemplateFile()")
+					print('*** handleTemplateFile()')
 					self.handleTemplateFile(templatePath)
 				else:
 					localPath = self.getLocalPath()
@@ -365,41 +362,41 @@ class Application ():
 
 
 	def handleDefaultPath (self):
-		self.response.body = ""
-		print("Handling default path")
+		self.response.body = ''
+		print('Handling default path')
 		self.setContentType('text/plain')
-		self.addResponseLine("[splunge]")
+		self.addResponseLine('[splunge]')
 
 
 	def handleFavicon (self):
-		errmsg = "The Favicon feature (favicon.ico) is not supported here, my friend"
+		errmsg = 'The Favicon feature (favicon.ico) is not supported here, my friend'
 		self.response.addHeader('Content-type', 'text/plain')
 		self.response.addHeader('Warning', errmsg)
 		self.response.statusCode = 410
 		self.response.statusMessage = errmsg
-		self.response.body = ""
+		self.response.body = ''
 
 
 	def handlePythonFile (self, path):
-		self.response.body = ""
+		self.response.body = ''
 		module = MagicLoader.loadModule(path)
-		print("module={}".format(module))
+		print('module={}'.format(module))
 		self.enrichModule(module)
 		args = execModule(module)
 		if '_' in args:
-			print("Underscore detected - handling shortcut response")
+			print('Underscore detected - handling shortcut response')
 			self.handleShortcutResponse(args)
 		else:
 			templatePath = self.inferTemplatePath()
 			if os.path.isfile(templatePath):
 				self.handleTemplateFile(templatePath, args)
 			else:
-  				print("template path was not found")
+  				print('template path was not found')
   				self.response.headers.append(('Content-Type', 'text/plain'))
   				for name in sorted(args):
   					value = args.get(name, '')
   					if not callable(value):
-  						self.response.body += "{} = {}\n".format(name, value)
+  						self.response.body += '{} = {}\n'.format(name, value)
 
 
 	def handleShortcutResponse (self, args):
@@ -413,41 +410,50 @@ class Application ():
 				s = str(underscore)
 				self.response.body = renderString(s, args)
 			else:
-				print("This is an object, so we are going to treat it like one")
+				print('This is an object, so we are going to treat it like one')
 				d = underscore.__dict__
 				pprint(d)
 				attrKeys = [key for key in d if not callable(d[key])]
-				self.addResponseLine("<table>")
+				self.addResponseLine('<table>')
 				for key in attrKeys:
 					val = d[key]
-					print("{}={}".format(key, val))
-					line = "<tr> <td>{}</td> <td>{}</td> </tr>".format(key, val)
+					print('{}={}'.format(key, val))
+					line = '<tr> <td>{}</td> <td>{}</td> </tr>'.format(key, val)
 					self.addResponseLine(line)
-				self.addResponseLine("</table>")
+				self.addResponseLine('</table>')
 
 
 	def handleStaticContent (self):
 		localPath = self.getLocalPath()
-		print("localPath={}".format(localPath))
+		print('localPath={}'.format(localPath))
 		(root, ext) = os.path.splitext(localPath)
-		defaultContentType = "application/octet-stream"
+		defaultContentType = 'application/octet-stream'
 		if not ext:
 			contentType = defaultContentType
 		else:			
 			contentType = types.map.get(ext, defaultContentType)
-		print("contentType={}".format(contentType))
+		print('contentType={}'.format(contentType))
 		self.setContentType(contentType)
 		content = open(localPath, 'rb', buffering=0).readall()
 		self.response.body = content
 
 
 	def handleTemplateFile (self, path, args=None):
-		print("templatePath={}".format(path))
-		print("About to execute template ...")
+		print('templatePath={}'.format(path))
+		print('About to execute template ...')
 		if not args:
 			args = {}
 			args['http'] = self.createHttpObject()
 		self.response.body = renderTemplate(path, args)
+
+
+	def respond (self):
+		status = self.response.getStatus()
+		if not self.response.exc_info:
+			self.startResponse(status, self.response.headers)
+		else:
+			self.startResponse(status, self.response.headers, self.response.exc_info)
+
 
 
 	@staticmethod
@@ -494,17 +500,17 @@ class Application ():
 
 	def addResponseLine (self, s):
 		self.response.body += s
-		self.response.body += "\r\n"
+		self.response.body += '\r\n'
 
 
 
 def argsToTuple (*args, length):
-	print("*** args: {}".format(args))
-	print("len(args)={}".format(len(args)))
+	print('*** args: {}'.format(args))
+	print('len(args)={}'.format(len(args)))
 	if len(args) == length:
 		t = tuple(args)
 	elif len(args) == 1 and isinstance(args[0], (list, tuple)) and len(args[0]) == length:
 		t = tuple(args[0])
 	else:
-		raise Exception("Arguments must either be {} value(s) or a list or tuple {} element(s) long".format(length, length))
+		raise Exception('Arguments must either be {} value(s) or a list or tuple {} element(s) long'.format(length, length))
 	return t
