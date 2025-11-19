fb11ebf3fb028d1a902220f448b2fc10507b9132
Chris Lalos <chris.lalos@gmail.com>
2025-11-19 09:46:51 -0600

    lookin good

--- test/test_markdown.py
+++ test/test_markdown.py
@@ -8,33 +8,53 @@ from splunge import app, constants, util, MarkdownHandler, Response
 
 class MarkdownTests(unittest.TestCase):
 	def test_hello(self):
+		# setup paths
 		localPath = 'www/hello.md'
 		urlPath = f'/{localPath}'
-		# Create a wsgi + then create a handler for it
-		wsgi = create_environ(urlPath)
-		handler = app.create_handler(wsgi)
-		self.assertIsNotNone(handler)
-		self.assertIsInstance(handler, MarkdownHandler)
-		# Execute the handler
-		(resp, isDone) = handler.handle_request(wsgi)
-		self.assertIsNotNone(resp)
-		self.assertIs(type(resp), Response)
-		self.assertTrue(isDone)
-		self.assertIsInstance(resp.iter, list)
-		self.assertEqual(1, len(resp.iter))	
-		self.assertIsNotNone(resp.iter[0])
-		content = resp.iter[0]
+		# create a handler for the path + execute it
+		content = handle_request(self, urlPath)
 		# Create expected output & test
 		content_expected = load_markdown_content(localPath)
 		self.assertEqual(content_expected, content)
 
 		
+
+def create_markdown_handler(testcase, path):
+	# Create a wsgi + then create a handler for it
+	wsgi = create_environ(path)
+	handler = app.create_handler(wsgi)
+	testcase.assertIsNotNone(handler)
+	testcase.assertIsInstance(handler, MarkdownHandler)
+	return handler
+
+
+def handle_request(testcase, path):
+	handler = create_markdown_handler(testcase, path)
+	wsgi = create_environ(path)
+	(resp, isDone) = handler.handle_request(wsgi)
+	testcase.assertIsNotNone(resp)
+	testcase.assertIs(type(resp), Response)
+	testcase.assertTrue(isDone)
+	testcase.assertIsInstance(resp.iter, list)
+	testcase.assertEqual(1, len(resp.iter))	
+	testcase.assertIsNotNone(resp.iter[0])
+	return resp.iter[0]
+
+
 def load_markdown_content(path):
+	''' Load the expected markdown content for the path.
+		
+	This is a concatenation of ...
+		- stock html_pre
+		- html conversion of the rstripped markdown loaded from the path
+		- stock html_post
+	The converted html's <title> tag is set to the path's filename.
+	'''
 	title = os.path.basename(path)
 	pre = constants.html_pre
 	post = constants.html_post
 	with open(path, 'r') as f:
 		frag = MarkdownIt().render(f.read())
-		content = util.html_fragment_to_doc(frag, title=title, pre=pre, post=post)	
+		content = util.html_fragment_to_doc(frag.rstrip(), title=title, pre=pre, post=post)	
 	return f'{content}\r\n'.encode('latin-1')
 	
