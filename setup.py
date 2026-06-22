b2dc44276c31b4d8881b0f326a8aff2418a57bda
Chrissy <chris@dylt.dev>
2026-06-22 06:52:24 +0000

    fixed

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ setuptools.setup(name='splunge',
                      'gunicorn',
                      'jinja2',
                      'markdown_it_py',
-                     'pygments'
+                     'pygments',
+                     'werkzeug',
                  ],
                  scripts=['scripts/www'],
+                 package_dir={'': 'src'},
+                 packages=setuptools.find_packages(where='src'),
                  version='0.1.1',
                  )
-#                 packages=setuptools.find_packages(exclude=['tests']))
